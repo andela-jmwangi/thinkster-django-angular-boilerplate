4eda1ab239cc3434d57a5031bd41026f21a9cff1
“JackMwangi” <jackmwa94@gmail.com>
2016-04-23 12:16:04 +0300

    (Chore) add AccountManger model

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class AccountManger(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('Users must have a valid email address.')
+        if not kwargs.get('username'):
+            raise ValueError('Users must have a valid username')
+
+        account = self.model(
+            email=self.normalize_email(email), username=kwargs.get('username'))
+        account.set_password(password)
+        account.save()
+
+        return account
+
+    def create_superuser(self, email, pass, **kwargs):
+        account = self.create_superuser(email, pass, **kwargs)
+
+        account.is_admin = True
+        account.save()
+
+        return account
 
 
 class Account(AbstractBaseUser):
